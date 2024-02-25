08adee058eaab763a9f8a1fc2652eb5bd232da9b
Andre Luis <andre.1357.luis@gmail.com>
2024-02-25 15:54:10 -0300

    🗃️ Create migration to create database

---
+++ src/infra/db/migrations/20240225182852_create_table_students.js
@@ -0,0 +1,13 @@
+export function up(knex) {
+  return knex.schema.createTable('students', (table) => {
+    table.increments('id').primary();
+    table.string('nome').notNullable();
+    table.string('email').notNullable();
+    table.integer('ra').notNullable().unique();
+    table.string('cpf').notNullable();
+  });
+}
+
+export function down(knex) {
+  return knex.schema.dropTable('students');
+}
