b83c262eb2a956c7f3ef359d097888e95043e7b9
Andre Luis <andre.1357.luis@gmail.com>
2024-03-03 13:55:27 -0300

    ✨ Name cannot have white space

--- src/resolvers/Mutation/student.js
+++ src/resolvers/Mutation/student.js
@@ -1,6 +1,11 @@
-export function createStudent(parent, args, context, info) {
-  return context.prisma.createStudent({
-    name: args.name,
-    email: args.email,
-  });
+import { ValidationError } from '../../support/errors.js';
+
+export async function createStudent(parent, args, context, info) {
+  const { name } = args.input ?? {};
+
+  if (!name.trim()) {
+    return Promise.reject(ValidationError.build('Invalid.whitespace'));
+  }
+
+  return null;
 }
