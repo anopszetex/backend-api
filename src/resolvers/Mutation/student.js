cb5df02500e31e1a4f8884ff9e5f4ad053146398
Andre Luis <andre.1357.luis@gmail.com>
2024-03-03 15:00:58 -0300

    🦺 Email cannot be undefined and contains white space

--- src/resolvers/Mutation/student.js
+++ src/resolvers/Mutation/student.js
@@ -1,8 +1,11 @@
 import { ValidationError } from '../../support/errors.js';
 
-export async function createStudent(parent, args, context, info) {
-  const { name } = args.input ?? {};
-
+/**
+ * Validates the given name.
+ * @param {string} name - The name to be validated.
+ * @return {Promise} A promise that resolves if the name is valid, or rejects with a validation error.
+ */
+async function validateName(name) {
   if (!name) {
     return Promise.reject(ValidationError.build('Invalid.name'));
   }
@@ -13,6 +16,20 @@ export async function createStudent(parent, args, context, info) {
 
   if (name.length > 100) {
     return Promise.reject(ValidationError.build('Invalid.length'));
+  }
+}
+
+export async function createStudent(parent, args, context, info) {
+  const { name, email } = args.input ?? {};
+
+  await validateName(name);
+
+  if (!email) {
+    return Promise.reject(ValidationError.build('Invalid.email'));
+  }
+
+  if (!email.trim()) {
+    return Promise.reject(ValidationError.build('Invalid.emailWithWhitespace'));
   }
 
   return null;
