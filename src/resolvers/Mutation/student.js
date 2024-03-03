4696000596459264d4483a1ff6401ac07b169345
Andre Luis <andre.1357.luis@gmail.com>
2024-03-03 14:41:30 -0300

    🦺 Name cannot be null or undefined

--- src/resolvers/Mutation/student.js
+++ src/resolvers/Mutation/student.js
@@ -3,6 +3,10 @@ import { ValidationError } from '../../support/errors.js';
 export async function createStudent(parent, args, context, info) {
   const { name } = args.input ?? {};
 
+  if (!name) {
+    return Promise.reject(ValidationError.build('Invalid.name'));
+  }
+
   if (!name.trim()) {
     return Promise.reject(ValidationError.build('Invalid.whitespace'));
   }
