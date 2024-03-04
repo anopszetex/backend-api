5dfaa0dbb2e274b2a677db9f644c016eaba72848
Andre Luis <andre.1357.luis@gmail.com>
2024-03-04 08:12:47 -0300

    ✅ Email cannot have more than 100 characters

--- src/tests/unit/student/add.test.js
+++ src/tests/unit/student/add.test.js
@@ -102,6 +102,21 @@ describe('Mutation to add a student', () => {
     }
   });
 
+  it('Email cannot have more than 100 characters', async () => {
+    const input = {
+      name: 'any_name',
+      email: 'a'.repeat(101),
+      cpf: '12345678900',
+      ra: '123456',
+    };
+
+    try {
+      await createStudent({}, { input }, {});
+    } catch (error) {
+      expectError(error, 'Invalid.EmailLength');
+    }
+  });
+
   it('Email must have a valid format (e.g. demo@demo.com', async () => {
     const input = {
       name: 'any_name',
@@ -117,21 +132,23 @@ describe('Mutation to add a student', () => {
     }
   });
 
-  it('Email cannot have more than 100 characters', async () => {
+  it('CPF cannot be null', () => {
     const input = {
       name: 'any_name',
-      email: 'a'.repeat(101),
-      cpf: '12345678900',
+      email: 'any_email@.com',
+      cpf: null,
       ra: '123456',
     };
 
     try {
       await createStudent({}, { input }, {});
     } catch (error) {
-      expectError(error, 'Invalid.EmailLength');
+      expectError(error, 'Invalid.cpf');
     }
   });
-  it.todo('CPF cannot have white space');
+
+  it.todo('CPF cannot have white space', () => {});
+
   it.todo('CPF must have 11 characters');
   it.todo('CPF must be unique');
 
