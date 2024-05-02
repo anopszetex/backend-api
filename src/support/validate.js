a19b81cc1f70d993302d5dec5aa18ec985f15c43
Andre Luis <andre.1357.luis@gmail.com>
2024-05-01 22:44:15 -0300

    ✨ Add validate and sanitize cpf

--- src/support/validate.js
+++ src/support/validate.js
@@ -1,5 +1,16 @@
 const EMAIL_REGEX = /^\S+@\S+\.\S+$/;
 
+const REGEX_REMOVE_SPECIAL_CHARS = /\.|-/g;
+const REGEX_FORMAT_CPF = /^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$/;
+
 export function isValidEmail(email) {
   return EMAIL_REGEX.test(email);
+}
+
+export function isValidFormat(cpf) {
+  return REGEX_FORMAT_CPF.test(cpf);
+}
+
+export function sanitizeCpf(cpf) {
+  return String(cpf).replace(REGEX_REMOVE_SPECIAL_CHARS, '');
 }
