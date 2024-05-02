2bd716ccd07f381f523495ea4f3e9cc7e5f3e50a
Andre Luis <andre.1357.luis@gmail.com>
2024-05-01 22:44:48 -0300

    🚚 Export functions

--- src/support/index.js
+++ src/support/index.js
@@ -1,8 +1,8 @@
 import { ValidationError } from './errors.js';
-import { isValidEmail } from './validate.js';
+import { isValidEmail, isValidFormat, sanitizeCpf } from './validate.js';
 
 export function __dirname(relativePath) {
   return new URL('.', relativePath).pathname;
 }
 
-export { ValidationError, isValidEmail };
+export { ValidationError, isValidEmail, isValidFormat, sanitizeCpf };
