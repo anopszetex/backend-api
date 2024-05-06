5ffe43195170b7030d48ebc20e8993b7cca8e8de
Andre Luis <andre.1357.luis@gmail.com>
2024-05-05 22:48:22 -0300

    ♻️ Update default value

--- src/infra/db/config.js
+++ src/infra/db/config.js
@@ -1,6 +1,6 @@
 export const getDatabaseConfig = Object.freeze({
   DB_HOST: process.env.DB_HOST ?? 'localhost',
-  DB_PORT: process.env.DB_PORT ?? 5432,
+  DB_PORT: process.env.DB_PORT ?? 5433,
   DB_USER: process.env.DB_USER ?? 'root1',
   DB_PASSWORD: process.env.DB_PASSWORD ?? 'root1',
   DB_NAME: process.env.DB_NAME ?? 'students-dev',
